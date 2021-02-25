4ad15e8dc609ab72f4ea883c011630089c5e1e74
seunsamueldcompguy <seun.samuel.dcomputer.guy@gmail.com>
2021-02-25 04:49:57 -0930

    prev, next and random person

--- src/components/review.js
+++ src/components/review.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import data from "./data";
 import {
   FaChevronLeft,
@@ -8,9 +8,44 @@ import {
   FaQuoteRight,
 } from "react-icons/fa";
 
+const checkNumber = (num) => {
+  if (num > data.length - 1) {
+    return 0;
+  }
+  if (num < 0) {
+    return data.length - 1;
+  } else {
+    return num;
+  }
+};
 const Review = () => {
-  const [index, setIndex] = useState(3);
+  const [index, setIndex] = useState(2);
   const { name, job, image, text } = data[index];
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const randomPerson = () => {
+    let randomPerson = Math.floor(
+      Math.random() * data.length
+    );
+    if (randomPerson === index) {
+      randomPerson = index + 1;
+    }
+    setIndex(checkNumber(randomPerson));
+    console.log(randomPerson);
+  };
 
   return (
     <article className="review">
@@ -29,13 +64,19 @@ const Review = () => {
       <p className="info">{text}</p>
       <div className="button-container">
         <button className="prev-btn">
-          <FaChevronLeft></FaChevronLeft>
+          <FaChevronLeft
+            onClick={prevPerson}
+          ></FaChevronLeft>
         </button>
         <button className="prev-btn">
-          <FaChevronRight></FaChevronRight>
+          <FaChevronRight
+            onClick={nextPerson}
+          ></FaChevronRight>
         </button>
       </div>
-      <button className="random-btn">surprise me</button>
+      <button className="random-btn" onClick={randomPerson}>
+        surprise me
+      </button>
     </article>
   );
 };
